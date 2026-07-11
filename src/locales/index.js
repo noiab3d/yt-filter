228828f89ab858736b8ac429dd3c3666310158e7
David Serrano <davidsrno96@hotmail.com>
2026-07-12 00:31:32 +0100

    Update index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,7 +2,7 @@ import { strings as pt } from './pt.js';
 import { strings as en } from './en.js';
 
 const LOCALES = { pt, en };
-const DEFAULT_LOCALE = 'pt';
+const DEFAULT_LOCALE = 'en';
 
 // Deteção automática via navigator.language, sem seletor manual na UI
 // (decisão confirmada com o David, 2026-07-11). Se o idioma do browser não
